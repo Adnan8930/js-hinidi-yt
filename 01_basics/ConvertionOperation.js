ec45acea82895cacc26c13383b4e4fa2cdf797d3
Adnan8930 <168887427+Adnan8930@users.noreply.github.com>
2024-12-19 06:08:18 +0000

    Convertion of datatype

---
+++ 01_basics/ConvertionOperation.js
@@ -0,0 +1,24 @@
+// number converstion
+let score = "33"
+let sc=Number(score)
+console.log(sc);
+// "33" => 33
+// "33ab" => NaN
+// true => 1; fasle => 0
+
+//Boolean converstion
+let isBoolean = "adnan"
+let BooleanIS=Boolean(isBoolean)
+console.log(BooleanIS);
+//  1=> true;0=> false
+//  "" => false ; "adnan"=>true
+
+//string converstion
+let Str = true
+let StrNum= String(Str)
+console.log(typeof StrNum);
+console.log(StrNum);
+//  33=> "33" 
+//true => "true"; false=>"false"
+
+
