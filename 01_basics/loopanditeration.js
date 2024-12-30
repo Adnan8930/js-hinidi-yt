27acee4724fda0a46454f2ce30dcc0421b484ce0
Adnan8930 <168887427+Adnan8930@users.noreply.github.com>
2024-12-30 06:41:59 +0000

    loops / break and continue

---
+++ 01_basics/loopanditeration.js
@@ -0,0 +1,34 @@
+//loop and iteration
+
+//1:for loop
+// for(let i=0; i<=5;i++){
+//     // console.log("hello world");
+    
+// }
+
+//do..while
+
+// let i=0;
+// do{
+//     i+=1;
+//     // console.log(i);
+// }while(i<5)
+
+// //while statement
+// let n=0;
+// while(n<5){
+//     // console.log('hello');
+//     n+=1;
+// }
+
+// let myArray =["flash" , "spiderman", "batman"]
+// console.log(myArray.length);
+
+for (let index = 1; index < 10; index++) {
+    if (index==5) {
+        console.log(`detectd ${index}`);    
+       continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
