d587892c60ac6869c381148b08059c594ae08b4b
Adnan8930 <168887427+Adnan8930@users.noreply.github.com>
2025-01-05 05:50:19 +0000

    string done

--- 01_basics/01_string.js
+++ 01_basics/01_string.js
@@ -1,17 +1,24 @@
-const name ="adnan-m"
+const name ="adnan madar"
 
 
 //console.log(name.__proto__.lentgh);it is used access the prototype of obbect
 // there are multiple proto type ex:length,at(),big,small,lastIndexOf ,
-// chatAt ,indexOf, etc..
+// chatAt ,indexOf, etc.. 
 
 console.log(name.indexOf('n'));
 const newString = name.substring(3)
 console.log(newString);
 
-const anotherString = name.slice(-5,-4)
-// console.log(anotherString);
+const anotherString = name.slice(-8,8)
+console.log(anotherString);
 
 let nam ="Hello World"
 console.log(nam.replace(/HEllo/i,"world"));//concat it is use to add two string used instead + operator
+
+console.log("adnan\nmadar ");
+let myname="    adnan \n "
+console.log(myname.trim());
+
+console.log(myname.includes('adnan'));
+console.log(name.split(' '))
 
