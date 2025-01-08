25839d33272391a6c784e24b8d29113fe94d5529
Adnan8930 <168887427+Adnan8930@users.noreply.github.com>
2025-01-08 14:00:42 +0000

    01_basics Done

--- 01_basics/NumAndMath.js
+++ 01_basics/NumAndMath.js
@@ -1,19 +1,37 @@
 const score=350
-console.log(score);
+// console.log(score);
 
 const newscore =new Number(100)
-console.log(newscore);
+// console.log(newscore);
 
-console.log(newscore.toString().length);
+// console.log(newscore.toString().length);
 
 const value =4500
-console.log(value.toFixed(2));
+// console.log(value.toFixed(2));
 
 const nextScore= 367785.5
-console.log(nextScore.toPrecision(3));
+// console.log(nextScore.toPrecision(3));
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-KR'));
+// console.log(hundreds.toLocaleString('en-KR'));
 
 const date = new Date(Date.UTC(2012,11,20,3,0,0))
-console.log(date.toLocaleString('en-IN' ,{timeZone: 'UTC'}));
+// console.log(date.toLocaleString('en-IN' ,{timeZone: 'UTC'}));
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(49));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+
